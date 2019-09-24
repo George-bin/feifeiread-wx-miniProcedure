9d50e17f360438107feb474cfae681d14f5e812c
gengshaobin <770021714@qq.com>
2019-09-24 18:35:37 +0800

    no message

--- pages/bookrack/bookrack.js
+++ pages/bookrack/bookrack.js
@@ -22,7 +22,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.handleGetBookRackInfo()
   },
 
   /**
@@ -37,7 +37,6 @@ Page({
    */
   onShow: function () {
     this.init()
-    this.handleGetBookRackInfo()
   },
 
   /**
